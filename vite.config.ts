bc47db43d98dd31c4253ab7bdb54cfff0b9928f9
genXstark <stark@nochef.ca>
2026-02-25 18:47:05 -0800

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const projectRoot = process.env.PROJECT_ROOT || __dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cipher-sl-cosmetica-website/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
